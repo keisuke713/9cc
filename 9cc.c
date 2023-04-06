f749ff57c348a09166582902268e1d5167e8d744
keisuke713 <nebakei.tkb713@gmail.com>
2023-04-06 20:38:04 +0900

    単項演算子を追加

--- 9cc.c
+++ 9cc.c
@@ -160,6 +160,7 @@ Node *new_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -176,18 +177,27 @@ Node *expr() {
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" prunaryimary)*
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_binary(ND_MUL, node, primary());
+            node = new_binary(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_binary(ND_DIV, node, primary());
+            node = new_binary(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+// unary = ("+" } "-")? unary
+Node *unary() {
+    if (consume('+'))
+        return unary();
+    if (consume('-'))
+        return new_binary(ND_SUB, new_num(0), unary());
+    return primary();
 }
 
 // primary = "(" expr ")" | num
